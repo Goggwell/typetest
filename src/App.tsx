309998903c8f6c974f5f1701992ff1b30ba0fa90
samuel-bd <samuely@frisseblikken.com>
2022-01-28 16:52:56 +0700

    recordTest() switch cases

--- src/App.tsx
+++ src/App.tsx
@@ -57,8 +57,81 @@ const App = () => {
         return;
       }
       if (timeUp === null && e.key !== 'Tab') startTimer();
+      const currIdx = wordList.indexOf(currWord);
+      const currWordEl = document.getElementById('active')!;
+      currWordEl.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      const caret = document.getElementById('caret')!;
+      caret.classList.remove('blink');
+      setTimeout(() => caret?.classList.add('blink'), 500);
+      switch (e.key) {
+        case 'Tab':
+          if (timer !== timeLimit || timeUp) {
+            resetTest();
+            document.getElementsByClassName('word')[0].scrollIntoView();
+          }
+          e.preventDefault();
+          break;
+        case ' ':
+          if (typedWord === '') return;
+          currWordEl.classList.add(typedWord !== currWord ? 'wrong' : 'right');
+          setTypedWord('');
+          setCurrWord(wordList[currIdx + 1]);
+          setTypedHistory([...typedHistory, typedWord]);
+          break;
+        case 'Backspace':
+          if (
+            typedWord.length === 0 &&
+            typedHistory[currIdx - 1] !== wordList[currIdx - 1]
+          ) {
+            setCurrWord(wordList[currIdx - 1]);
+            setTypedWord(!e.ctrlKey ? typedHistory[currIdx - 1] : '');
+            setTypedHistory(typedHistory.splice(0, typedHistory.length - 1));
+            currWordEl.previousElementSibling!.classList.remove(
+              'right',
+              'wrong'
+            );
+            if (e.ctrlKey) {
+              currWordEl.previousElementSibling!.childNodes.forEach((char) => {
+                if (char instanceof HTMLSpanElement)
+                  char.classList.remove('wrong', 'right');
+              });
+            }
+          } else {
+            if (e.ctrlKey) {
+              setTypedWord('');
+              currWordEl.childNodes.forEach((char) => {
+                if (char instanceof HTMLSpanElement)
+                  char.classList.remove('wrong', 'right');
+              });
+            } else {
+              setTypedWord(typedWord.slice(0, typedWord.length - 1));
+              let idx = typedWord.length;
+              if (idx < currWord.length) {
+                currWordEl.children[idx + 1].classList.remove('wrong', 'right');
+              }
+            }
+          }
+          break;
+        default:
+          setTypedWord(typedWord + e.key);
+          let idx = typedWord.length - 1;
+          currWordEl.children[idx + 1].classList.add(
+            currWord[idx] !== typedWord[idx] ? 'wrong' : 'right'
+          );
+          break;
+      }
     },
-    [resetTest, startTimer, timeUp, timer]
+    [
+      resetTest,
+      startTimer,
+      timeUp,
+      timer,
+      currWord,
+      wordList,
+      timeLimit,
+      typedHistory,
+      typedWord,
+    ]
   );
 
   const changeTimeLimit = (newLimit: number) => {
