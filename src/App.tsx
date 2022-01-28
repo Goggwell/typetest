2fb861209bfcb82d7151f3821ef70b2adabd8899
samuel-bd <samuely@frisseblikken.com>
2022-01-28 14:30:03 +0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useLayoutEffect } from 'react';
+import { words } from './assets/words.json';
 import './App.scss';
 
-function App() {
-  return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface State {
+  currWord: string;
+  typedWord: string;
+  timer: number;
+  timeUp: NodeJS.Timeout | null;
+  timeLimit: number;
+  typedHistory: string[];
 }
 
-export default App;
+export const App = () => {
+  const wordList = words.sort(() => Math.random() - 0.5);
+
+  const [currWord, setCurrWord] = useState<State['currWord']>(words[0]);
+  const [typedWord, setTypedWord] = useState<State['typedWord']>('');
+  const [timer, setTimer] = useState<State['timer']>(60);
+  const [timeUp, setTimeUp] = useState<State['timeUp']>(null);
+  const [timeLimit, setTimeLimit] = useState<State['timeLimit']>(60);
+  const [typedHistory, setTypedHistory] = useState<State['typedHistory']>([]);
+
+  const startTimer = () => {
+    const intervalId = setInterval(() => {
+      setTimer(timer - 1);
+    }, 1000);
+    setTimeUp(intervalId);
+  };
+
+  const recordTest = (e: KeyboardEvent) => {
+    if (timer === 0) {
+      if (e.key === 'Tab') {
+        e.preventDefault();
+      }
+      return;
+    }
+    if (timeUp === null && e.key !== 'Tab') startTimer();
+  };
+
+  useLayoutEffect(() => {
+    if (timer === 0 && timeUp) {
+      clearInterval(timeUp);
+      setTimeUp(null);
+    }
+  }, [timer, timeUp]);
+
+  return <></>;
+};
