38286dcfcc195d6492ce7ef782816fba5037777e
Goggwell <zulkarnaensamuel1@gmail.com>
2022-01-29 06:24:47 +0700

    stylesheets

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,69 +1,68 @@
-import React from 'react'
+import React from 'react';
 
 interface Props {
-    typedWord: string;
-    currWord: string;
-    timer: number;
-    typedHistory: string[];
-    words: string[];
+  typedWord: string;
+  currWord: string;
+  timer: number;
+  typedHistory: string[];
+  words: string[];
 }
 
 const Test = (props: Props) => {
-    const { typedWord, currWord, timer, typedHistory, words } = props;
-    let extraLetters = typedWord.slice(currWord.length).split("");
+  const { typedWord, currWord, timer, typedHistory, words } = props;
+  let extraLetters = typedWord.slice(currWord.length).split('');
 
-    return (
-        <div className="test">
-            <div className="timer">{timer}</div>
-            <div className="box">
-                {words.map((word, idx) => {
+  return (
+    <div className='test'>
+      <div className='timer'>{timer}</div>
+      <div className='box'>
+        {words.map((word, idx) => {
+          return (
+            <div
+              key={word + idx}
+              className='word'
+              id={currWord === word ? 'active' : undefined}
+            >
+              {currWord === word ? (
+                <span
+                  id='caret'
+                  className='blink'
+                  style={{
+                    left: typedWord.length * 14.8533,
+                  }}
+                >
+                  |
+                </span>
+              ) : null}
+              {word.split('').map((char, charId) => {
+                return <span key={char + charId}>{char}</span>;
+              })}
+              {currWord === word
+                ? extraLetters.map((char, charId) => {
                     return (
-                        <div
-                            key={word + idx}
-                            className="word"
-                            id={currWord === word ? "active" : undefined}
-                        >
-                            {currWord === word ? (
-                                <span
-                                    id="caret"
-                                    className="blink"
-                                    style={{
-                                        left: typedWord.length * 14.8533,
-                                    }}
-                                >
-                                    |
-                                </span>
-                            ) : null}
-                            {word.split("").map((char, charId) => {
-                                return (
-                                    <span key={char + charId}>{char}</span>
-                                );
-                            })}
-                            {currWord === word ? extraLetters.map((char, charId) => {
-                                return (
-                                    <span
-                                        key={char + charId}
-                                        className="wrong extra"
-                                    >
-                                        {char}
-                                    </span>
-                                );
-                            }) : typedHistory[idx] ? typedHistory[idx].slice(words[idx].length).split("").map((char, charId) => {
-                                return (
-                                    <span
-                                        key={char + charId}
-                                        className="wrong extra"
-                                    >
-                                        {char}
-                                    </span>
-                                );
-                            }) : null}
-                        </div>
+                      <span key={char + charId} className='wrong extra'>
+                        {char}
+                      </span>
                     );
-                })}
+                  })
+                : typedHistory[idx]
+                ? typedHistory[idx]
+                    .slice(words[idx].length)
+                    .split('')
+                    .map((char, charId) => {
+                      return (
+                        <span key={char + charId} className='wrong extra'>
+                          {char}
+                        </span>
+                      );
+                    })
+                : null}
             </div>
-        </div>
-    );
-}
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default Test;
